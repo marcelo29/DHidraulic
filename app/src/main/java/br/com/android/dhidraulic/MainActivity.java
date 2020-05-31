c893811e97b45db006f28032280e0dfad4d62056
masr <masasp29@gmail.com>
2020-05-31 17:07:23 -0300

    os linux

--- app/src/main/java/br/com/android/dhidraulic/MainActivity.java
+++ app/src/main/java/br/com/android/dhidraulic/MainActivity.java
@@ -1,14 +1,25 @@
 package br.com.android.dhidraulic;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import br.com.android.dhidraulic.databinding.ActivityMainBinding;
+import br.com.android.domain.EstruturaCasa;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtNumPavimentos, edtNumPessoas;
+    EstruturaCasa casa;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        casa = new EstruturaCasa();
+        //casa.setNumPavimentos();
     }
 }
